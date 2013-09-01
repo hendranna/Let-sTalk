6cea457d4936dc0c99ee0d52648913364738696c
Anna Hendre <hendrannah@gmail.com>
2013-09-01 22:09:28 +0100

    created the model for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130830154701) do
+ActiveRecord::Schema.define(:version => 20130901210847) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "friendships", :force => true do |t|
     t.integer  "user_id"
