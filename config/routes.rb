e3585a87c3b146d3957ecbcbe6e68f73fad7fe3e
Anna Hendre <hendrannah@gmail.com>
2013-09-02 11:37:34 +0100

    added kaminari tried to do the pagination

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Talk2me::Application.routes.draw do
     get '/sign_up', to: 'devise/sessions#destroy'
   end
   
-
   root to: "users#index"
 
   resources :users, except: [:new, :create, :update, :edit, :destroy]
+
   resources :users do
     resources :friendships
   end 
@@ -20,6 +20,10 @@ Talk2me::Application.routes.draw do
   resources :users do
     resources :comments
   end
+  
+  resources :users do 
+    get 'page/:page', action: :index, on: :collection
+  end
 
   match "/friendships" => "friendships#create", :via => :post, :as => :create_friendship
 
@@ -28,7 +32,7 @@ Talk2me::Application.routes.draw do
   match "/comments" => "comments#create", :via => :post, :as => :create_comment
 
 
-   match 'search', to: 'search#index', via: [:get, :post], as: :search
+  match 'search', to: 'search#index', via: [:get, :post], as: :search
 
 
   # The priority is based upon order of creation:
