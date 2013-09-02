08e49c730b77978f391d066365d0c22a7ee23a2a
Anna Hendre <hendrannah@gmail.com>
2013-09-02 11:00:52 +0100

    fixed the bugs for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,8 @@ class SearchController < ApplicationController
       redirect_to user_path
     else
       search = params[:q].to_s.downcase
-      @albums = Language.where('LOWER(name) like :search', search: "%#{search}%")
-      @users = User.where('LOWER(username) like :search OR LOWER(firstname) like :search OR LOWER(lastname) like :search OR LOWER(biography) like :search', search: "%#{search}%")
+      @languages = Language.where('LOWER(name) like :search', search: "%#{search}%")
+      @users = User.where('LOWER(firstname) like :search OR LOWER(lastname) like :search OR LOWER(biography) like :search', search: "%#{search}%")
     end
   end
 
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,6 @@
 class Language < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :user_languages_attributes
   #has_many :user_languages, dependent: :destroy
-  has_many :users, through: :user_language
-end
+  has_many :user_languages
+  has_many :users, through: :user_languages
+end 
