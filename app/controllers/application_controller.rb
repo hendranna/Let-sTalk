504b9ae4acdfdbbea3ea7310333b6a6e13b158dc
Anna Hendre <hendrannah@gmail.com>
2013-09-05 15:10:53 +0100

    changed the path in application controller forsign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to login_path, alert: "You shall not pass!"
+    redirect_to sign_in_path, alert: "You shall not pass!"
   end
 
 
-  protected
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to login_path, alert: "You shall not pass!"
-  end
+ 
 
 end
